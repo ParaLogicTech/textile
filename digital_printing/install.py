f805e1b264274e966e333ec578658139166a01d9
Saif Ur Rehman <saif@paralogic.io>
2023-05-15 16:48:15 +0500

    feat(Installer): Updated Fabric Type list

--- digital_printing/install.py
+++ digital_printing/install.py
@@ -1,40 +1,85 @@
 import frappe
 
 
-fabric_types = [
-	'Brocade', 'Heavy', 'Egyptian', 'Golden Organza', 'Cambric', 'Crinkle Chiffon', 'Upholstery',
-	'Interlock', 'Parachute', 'Velour', 'Raw', 'Slub', 'Latha', 'SwissFoil', 'Flat Back Mesh', 'Lawn', 'Chiffon',
-	'Twill', 'Jacquard', 'Spunbond', 'Heavy Twill', 'Charmeuse', 'Suede', 'Karandi', 'Organza', 'Bemberg', 'Satin',
-	'Fleece', 'Bemberg Chiffon', 'Medium Twill', 'Georgette', 'RTW Poly Satin', 'Coarse Weave', 'Medium Weave',
-	'Tissue', 'Heavy Felt', 'Katan', 'Herringbone', 'Raw Silk', 'Thai Silk', 'Dobby Net', 'Linen', 'Crepe', 'Valvet',
-	'Plain', 'Canvas', 'Moss Crepe', 'Jamawar', 'Net', 'Khaddar', 'Medium', 'Poplin', 'Monar'
+fabric_materials = [
+	('Viscose', 'VI'),
+	('Silk', 'SI'),
+	('Polyester', 'PL'),
+	('Cotton', 'CO'),
 ]
 
 
-fabric_materials = ['Viscose', 'Silk', 'Polyester', 'Cotton']
+fabric_types = [
+	('Bemberg', 'Be'),
+	('Brocade', 'Br'),
+	('Cambric', 'Ca'),
+	('Canvas', 'Cv'),
+	('Charmeuse', 'Cm'),
+	('Chiffon', 'Cf'),
+	('Crepe', 'Ce'),
+	('Crinkle', 'Cr'),
+	('Denim', 'De'),
+	('Dobby', 'Do'),
+	('Felt', 'Fe'),
+	('Fleece', 'Dl'),
+	('Gauze', 'Ga'),
+	('Georgette', 'Ge'),
+	('Jacquard', 'Ja'),
+	('Jamawar', 'Jw'),
+	('Karandi', 'Kr'),
+	('Khaddar', 'Kh'),
+	('Knit', 'Kn'),
+	('Lace', 'Lc'),
+	('Latha', 'Lt'),
+	('Lawn', 'La'),
+	('Linen', 'Li'),
+	('Muslin', 'Mu'),
+	('Net', 'Ne'),
+	('Nonwoven', 'Nw'),
+	('Organza', 'Or'),
+	('Parachute', 'Pa'),
+	('Percale', 'Pe'),
+	('Plain', 'Pl'),
+	('Poplin', 'Po'),
+	('Satin', 'Sa'),
+	('Silk', 'Si'),
+	('Slub', 'Sl'),
+	('Suede', 'Su'),
+	('Terry', 'Te'),
+	('Tissue', 'Ti'),
+	('Twill', 'Tw'),
+	('Upholstery', 'Up'),
+	('Velvet', 'Vv'),
+	('Voile', 'Vo'),
+]
 
 
 def after_install():
-	populate_fabric_type()
+	from digital_printing.digital_printing.doctype.print_order.print_order import update_conversion_factor_global_defaults
+
 	populate_fabric_material()
+	populate_fabric_type()
 	create_printing_uom()
-
-
-def populate_fabric_type():
-	for name in fabric_types:
-		if not frappe.db.exists("Fabric Type", name):
-			frappe.get_doc({
-				"doctype": "Fabric Type",
-				"fabric_type": name
-			}).save()
+	update_conversion_factor_global_defaults()
 
 
 def populate_fabric_material():
-	for name in fabric_materials:
+	for name, abbr in fabric_materials:
 		if not frappe.db.exists("Fabric Material", name):
 			frappe.get_doc({
 				"doctype": "Fabric Material",
-				"fabric_material": name
+				"fabric_material": name,
+				"abbreviation": abbr,
+			}).save()
+
+
+def populate_fabric_type():
+	for name, abbr in fabric_types:
+		if not frappe.db.exists("Fabric Type", name):
+			frappe.get_doc({
+				"doctype": "Fabric Type",
+				"fabric_type": name,
+				"abbreviation": abbr,
 			}).save()
 
 
