b19f027ab6a627ca56cc9f675740e46d2a21e164
Saif Ur Rehman <saif@paralogic.io>
2023-05-25 18:52:40 +0500

    fix: sync_fixtures before patching

--- digital_printing/patches/set_panel_based_qty.py
+++ digital_printing/patches/set_panel_based_qty.py
@@ -1,18 +1,19 @@
 import frappe
 from frappe.model.utils.rename_field import rename_field
+from frappe.utils.fixtures import sync_fixtures
 
 
 def execute():
+	sync_fixtures(app="digital_printing")
+
 	doctypes = ["Sales Order Item", "Delivery Note Item", "Sales Invoice Item"]
 
 	for dt in doctypes:
-		frappe.reload_doctype(dt)
 		frappe.delete_doc_if_exists("Custom Field", f"{dt}-show_panel_in_print")
 
 		if frappe.db.has_column(dt, 'show_panel_in_print'):
 			rename_field(dt, "show_panel_in_print", "panel_based_qty")
 
-	frappe.reload_doctype("Print Order Item")
 	for name in frappe.get_all("Print Order", pluck="name"):
 		doc = frappe.get_doc("Print Order", name)
 		doc.calculate_totals()
