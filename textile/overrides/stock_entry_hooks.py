4d43b2400421146578be7ef4fbeed5b4291bcab2
haiderk0987 <haiderk0987@gmail.com>
2023-08-10 18:04:22 +0500

    feat(Work Order): add-ons for finish_multiple_work_orders

--- textile/overrides/stock_entry_hooks.py
+++ textile/overrides/stock_entry_hooks.py
@@ -1,8 +1,14 @@
 import frappe
+from frappe import _
 from erpnext.stock.doctype.stock_entry.stock_entry import StockEntry
 
 
 class StockEntryDP(StockEntry):
+	def validate(self):
+		super().validate()
+		self.validate_fabric_printer()
+		self.validate_print_process()
+
 	def on_submit(self):
 		super().on_submit()
 		self.update_print_order_fabric_transfer_status()
@@ -23,6 +29,30 @@ class StockEntryDP(StockEntry):
 			print_order = frappe.get_doc("Print Order", self.print_order)
 			print_order.set_fabric_transfer_status(update=True)
 			print_order.notify_update()
+
+	def validate_fabric_printer(self):
+		if self.purpose != "Manufacture":
+			return
+
+		if self.get("print_order"):
+			if not self.get("fabric_printer"):
+				frappe.throw(_("Fabric Printer mandatory for Manufacture Stock Entry of Print Order"))
+		else:
+			self.fabric_printer = None
+
+	def validate_print_process(self):
+		if self.purpose != "Manufacture":
+			return
+		if not self.get("fabric_printer") or not self.get("work_order"):
+			return
+
+		printer_process = frappe.get_cached_value("Fabric Printer", self.fabric_printer, "process_item")
+		work_order_process = frappe.db.get_value("Work Order", self.work_order, "process_item", cache=1)
+
+		if printer_process and printer_process != work_order_process:
+			frappe.throw(_("Fabric Printer {0} is not allowed to manufacture using Process {1} in {2}").format(
+				self.fabric_printer, work_order_process, frappe.get_desk_link("Work Order", self.work_order)
+			))
 
 
 def update_stock_entry_from_work_order(stock_entry, work_order):
