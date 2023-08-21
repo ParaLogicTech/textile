4093270597aad50119c87c5c196ba881b052154c
Saif Ur Rehman <saif@paralogic.io>
2023-08-21 13:00:28 +0500

    feat: Fabric Pretreatment Module

--- textile/fabric_pretreatment/doctype/fabric_pretreatment_settings/fabric_pretreatment_settings.js
+++ textile/fabric_pretreatment/doctype/fabric_pretreatment_settings/fabric_pretreatment_settings.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, ParaLogic and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Fabric Printing Settings', {
+frappe.ui.form.on('Fabric Pretreatment Settings', {
 	onload: function(frm) {
 		erpnext.queries.setup_queries(frm, "Warehouse", function(fieldname) {
 			return erpnext.queries.warehouse(frm.doc);
---
+++ textile/fabric_pretreatment/doctype/fabric_pretreatment_settings/fabric_pretreatment_settings.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2023, ParaLogic and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class FabricPretreatmentSettings(Document):
+	def validate(self):
+		self.update_global_defaults()
+
+	def update_global_defaults(self):
+		global_default_fields = [
+			"default_pretreatment_source_warehouse",
+			"default_pretreatment_wip_warehouse",
+			"default_pretreatment_fg_warehouse"
+		]
+
+		for fn in global_default_fields:
+			frappe.db.set_default(fn, self.get(fn, ''))
