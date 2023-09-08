b6f2f72819b8fa69c466f6a4d82b23a63cab66c9
Wasiq-Ali <wasiqali878@gmail.com>
2023-09-08 15:04:31 +0500

    Textile.v14(Print Packing List): Add Filter Dynamically

--- textile/fabric_printing/report/print_packing_list/print_packing_list.js
+++ textile/fabric_printing/report/print_packing_list/print_packing_list.js
@@ -26,23 +26,6 @@ frappe.query_reports["Print Packing List"] = {
 			label: __("Customer"),
 			fieldtype: "Link",
 			options: "Customer",
-			on_change: () => {
-				var customer = frappe.query_report.get_filter_value('customer');
-				if (customer) {
-					frappe.db.get_value('Customer', customer, ["customer_name"], function(value) {
-						frappe.query_report.set_filter_value('customer_name', value["customer_name"]);
-					});
-				} else {
-					frappe.query_report.set_filter_value('customer_name', "");
-				}
-			},
-		},
-		{
-			fieldname: "customer_name",
-			label: __("Customer Name"),
-			fieldtype: "Data",
-			read_only: 1,
-			hidden: 1,
 		},
 		{
 			"fieldname":"print_order",
