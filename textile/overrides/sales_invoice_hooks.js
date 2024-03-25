f4721dc1c8c75b7ab95f758c32465510b473c9b8
shahSpider <aq.syed007@gmail.com>
2024-03-25 11:28:05 +0500

    fix: commonified the pricing dialogue code

--- textile/overrides/sales_invoice_hooks.js
+++ textile/overrides/sales_invoice_hooks.js
@@ -19,378 +19,8 @@ frappe.ui.form.on("Sales Invoice", {
 	},
 
 	onload: function(listview) {
-		listview.page.add_menu_item(__("Check Printing Rate"), () => {
-			let doc = {
-				price_list: frappe.defaults.get_global_default("selling_price_list"),
-				applied_rules: [],
-			};
-
-			const get_price = () => {
-				if (doc.price_list && doc.fabric_item) {
-					return frappe.call({
-						method: "textile.fabric_printing.doctype.print_pricing_rule.print_pricing_rule.get_printing_rate_breakup",
-						args: {
-							item_code: doc.fabric_item,
-							price_list: doc.price_list,
-							customer: doc.customer
-						},
-						callback: (r) => {
-							if (r.message) {
-								doc.base_printing_rate = r.message.base_rate;
-								doc.printing_rate = r.message.rule_rate;
-								doc.fabric_rate = r.message.fabric_rate;
-								doc.price_list_rate = r.message.price_list_rate;
-								doc.applied_rules = r.message.applied_rules || [];
-								dialog.refresh();
-							}
-						}
-					});
-				} else {
-					doc.base_printing_rate = null;
-					doc.printing_rate = null;
-					doc.fabric_rate = null;
-					doc.price_list_rate = null;
-					doc.applied_rules = [];
-					dialog.refresh();
-				}
-			}
-
-			const get_fabric_item_name = () => {
-				if (doc.fabric_item) {
-					frappe.db.get_value("Item", doc.fabric_item, "item_name", (r) => {
-						if (r) {
-							dialog.set_value("fabric_item_name", r.item_name);
-						}
-					});
-				} else {
-					dialog.set_value("fabric_item_name", null);
-				}
-			}
-
-			const get_customer_name = () => {
-				if (doc.customer) {
-					frappe.db.get_value("Customer", doc.customer, "customer_name", (r) => {
-						if (r) {
-							dialog.set_value("customer_name", r.customer_name);
-						}
-					});
-				} else {
-					dialog.set_value("customer_name", null);
-				}
-			}
-
-			const dialog = new frappe.ui.Dialog({
-				title: __("Check Printing Rate"),
-				fields: [
-					{
-						label: __("Fabric Item Code"),
-						fieldname: "fabric_item",
-						fieldtype: "Link",
-						options: "Item",
-						reqd: 1,
-						get_query: () => erpnext.queries.item({
-							textile_item_type: ["in", ["Ready Fabric", "Greige Fabric"]],
-						}),
-						onchange: () => {
-							get_price();
-							get_fabric_item_name();
-						},
-					},
-					{
-						label: __("Fabric Item Name"),
-						fieldname: "fabric_item_name",
-						depends_on: "eval:doc.fabric_item && doc.fabric_item_name != doc.fabric_item",
-						fieldtype: "Data",
-						read_only: 1,
-					},
-					{
-						label: __("Customer"),
-						fieldname: "customer",
-						fieldtype: "Link",
-						options: "Customer",
-						onchange: () => {
-							get_price();
-							get_customer_name();
-						},
-					},
-					{
-						label: __("Customer Name"),
-						fieldname: "customer_name",
-						fieldtype: "Data",
-						depends_on: "eval:doc.customer && doc.customer_name != doc.customer",
-						read_only: 1,
-					},
-					{
-						label: __("Price List"),
-						fieldname: "price_list",
-						fieldtype: "Link",
-						options: "Price List",
-						reqd: 1,
-						get_query: () => {
-							return {
-								filters: {selling: 1}
-							}
-						},
-						onchange: () => {
-							get_price();
-						},
-					},
-					{
-						label: __("Rate"),
-						fieldtype: "Section Break",
-					},
-					{
-						label: __("Base Printing Rate"),
-						fieldname: "base_printing_rate",
-						fieldtype: "Currency",
-						read_only: 1,
-					},
-					{
-						label: __("Printing Rate"),
-						fieldname: "printing_rate",
-						fieldtype: "Currency",
-						read_only: 1,
-					},
-					{
-						label: __("Fabric Rate"),
-						fieldname: "fabric_rate",
-						fieldtype: "Currency",
-						read_only: 1,
-					},
-					{
-						label: __("Price List Rate"),
-						fieldname: "price_list_rate",
-						fieldtype: "Currency",
-						read_only: 1,
-						bold: 1
-					},
-					{
-						fieldtype: "Section Break",
-					},
-					{
-						label: __("Applied Rules"),
-						fieldname: "applied_rules",
-						fieldtype: "Table",
-						read_only: 1,
-						cannot_add_rows: 1,
-						get_data: () => doc.applied_rules,
-						fields: [
-							{
-								label: __("Rule"),
-								fieldname: "rule",
-								fieldtype: "Link",
-								options: "Print Pricing Rule",
-								read_only: 1,
-								in_list_view: 1
-							},
-							{
-								label: __("Type"),
-								fieldname: "type",
-								fieldtype: "Data",
-								read_only: 1,
-								in_list_view: 1
-							},
-							{
-								label: __("Value"),
-								fieldname: "value",
-								fieldtype: "Float",
-								read_only: 1,
-								in_list_view: 1
-							},
-						]
-					},
-				],
-				doc: doc,
-			});
-			dialog.show();
-		});
-
-		// pretreatment pricing rule dialogue
-		listview.page.add_menu_item(__("Check Pretreatment Rate"), () => {
-			let doc = {
-				price_list: frappe.defaults.get_global_default("selling_price_list"),
-				applied_rules: [],
-			};
-
-			const get_price = () => {
-				if (doc.price_list && doc.fabric_item) {
-					return frappe.call({
-						method: "textile.fabric_pretreatment.doctype.pretreatment_pricing_rule.pretreatment_pricing_rule.get_pretreatment_rate_breakup",
-						args: {
-							item_code: doc.fabric_item,
-							price_list: doc.price_list,
-							customer: doc.customer
-						},
-						callback: (r) => {
-							if (r.message) {
-								doc.base_pretreatment_rate = r.message.base_rate;
-								doc.pretreatment_rate = r.message.rule_rate;
-								doc.fabric_rate = r.message.fabric_rate;
-								doc.price_list_rate = r.message.price_list_rate;
-								doc.applied_rules = r.message.applied_rules || [];
-								dialog.refresh();
-							}
-						}
-					});
-				} else {
-					doc.base_pretreatment_rate = null;
-					doc.pretreatment_rate = null;
-					doc.fabric_rate = null;
-					doc.price_list_rate = null;
-					doc.applied_rules = [];
-					dialog.refresh();
-				}
-			}
-
-			const get_fabric_item_name = () => {
-				if (doc.fabric_item) {
-					frappe.db.get_value("Item", doc.fabric_item, "item_name", (r) => {
-						if (r) {
-							dialog.set_value("fabric_item_name", r.item_name);
-						}
-					});
-				} else {
-					dialog.set_value("fabric_item_name", null);
-				}
-			}
-
-			const get_customer_name = () => {
-				if (doc.customer) {
-					frappe.db.get_value("Customer", doc.customer, "customer_name", (r) => {
-						if (r) {
-							dialog.set_value("customer_name", r.customer_name);
-						}
-					});
-				} else {
-					dialog.set_value("customer_name", null);
-				}
-			}
-
-			const dialog = new frappe.ui.Dialog({
-				title: __("Check Pretreatment Rate"),
-				fields: [
-					{
-						label: __("Fabric Item Code"),
-						fieldname: "fabric_item",
-						fieldtype: "Link",
-						options: "Item",
-						reqd: 1,
-						get_query: () => erpnext.queries.item({
-							textile_item_type: ["in", ["Ready Fabric", "Greige Fabric"]],
-						}),
-						onchange: () => {
-							get_price();
-							get_fabric_item_name();
-						},
-					},
-					{
-						label: __("Fabric Item Name"),
-						fieldname: "fabric_item_name",
-						depends_on: "eval:doc.fabric_item && doc.fabric_item_name != doc.fabric_item",
-						fieldtype: "Data",
-						read_only: 1,
-					},
-					{
-						label: __("Customer"),
-						fieldname: "customer",
-						fieldtype: "Link",
-						options: "Customer",
-						onchange: () => {
-							get_price();
-							get_customer_name();
-						},
-					},
-					{
-						label: __("Customer Name"),
-						fieldname: "customer_name",
-						fieldtype: "Data",
-						depends_on: "eval:doc.customer && doc.customer_name != doc.customer",
-						read_only: 1,
-					},
-					{
-						label: __("Price List"),
-						fieldname: "price_list",
-						fieldtype: "Link",
-						options: "Price List",
-						reqd: 1,
-						get_query: () => {
-							return {
-								filters: {selling: 1}
-							}
-						},
-						onchange: () => {
-							get_price();
-						},
-					},
-					{
-						label: __("Rate"),
-						fieldtype: "Section Break",
-					},
-					{
-						label: __("Base Pretreatment Rate"),
-						fieldname: "base_pretreatment_rate",
-						fieldtype: "Currency",
-						read_only: 1,
-					},
-					{
-						label: __("Pretreatment Rate"),
-						fieldname: "pretreatment_rate",
-						fieldtype: "Currency",
-						read_only: 1,
-					},
-					{
-						label: __("Fabric Rate"),
-						fieldname: "fabric_rate",
-						fieldtype: "Currency",
-						read_only: 1,
-					},
-					{
-						label: __("Price List Rate"),
-						fieldname: "price_list_rate",
-						fieldtype: "Currency",
-						read_only: 1,
-						bold: 1
-					},
-					{
-						fieldtype: "Section Break",
-					},
-					{
-						label: __("Applied Rules"),
-						fieldname: "applied_rules",
-						fieldtype: "Table",
-						read_only: 1,
-						cannot_add_rows: 1,
-						get_data: () => doc.applied_rules,
-						fields: [
-							{
-								label: __("Rule"),
-								fieldname: "rule",
-								fieldtype: "Link",
-								options: "Pretreatment Pricing Rule",
-								read_only: 1,
-								in_list_view: 1
-							},
-							{
-								label: __("Type"),
-								fieldname: "type",
-								fieldtype: "Data",
-								read_only: 1,
-								in_list_view: 1
-							},
-							{
-								label: __("Value"),
-								fieldname: "value",
-								fieldtype: "Float",
-								read_only: 1,
-								in_list_view: 1
-							},
-						]
-					},
-				],
-				doc: doc,
-			});
-			dialog.show();
-		});
+		pricing_dialogue(listview, "Check Printing Rate", "textile.fabric_printing.doctype.print_pricing_rule.print_pricing_rule.get_printing_rate_breakup");
+		pricing_dialogue(listview, "Check Pretreatment Rate", "textile.fabric_pretreatment.doctype.pretreatment_pricing_rule.pretreatment_pricing_rule.get_pretreatment_rate_breakup");
 	}
 });
 
@@ -432,3 +62,206 @@ frappe.ui.form.on("Printed Fabric Detail", {
 		}
 	}
 });
+
+var pricing_dialogue = function(listview, title, method_path) {
+	listview.page.add_menu_item(__(title), () => {
+		let doc = {
+			price_list: frappe.defaults.get_global_default("selling_price_list"),
+			applied_rules: [],
+		};
+
+		let base_rate_field_name = title ==='Check Printing Rate' ? "base_printing_rate" : "base_pretreatment_rate";
+		let base_rate_field_label = title ==='Check Printing Rate' ? "Base Printing Rate" : "Base Pretreatment Rate";
+		let rate_field_name = title ==='Check Printing Rate' ? "printing_rate" : "pretreatment_rate";
+		let rate_field_label = title ==='Check Printing Rate' ? "Printing Rate" : "Pretreatment Rate";
+
+		const get_price = () => {
+			if (doc.price_list && doc.fabric_item) {
+				return frappe.call({
+					method: method_path,
+					args: {
+						item_code: doc.fabric_item,
+						price_list: doc.price_list,
+						customer: doc.customer
+					},
+					callback: (r) => {
+						if (r.message) {							
+							if (title === 'Check Printing Rate') {
+								doc.base_printing_rate = r.message.base_rate;
+								doc.printing_rate = r.message.rule_rate;
+							} else {
+								doc.base_pretreatment_rate = r.message.base_rate;
+								doc.pretreatment_rate = r.message.rule_rate;
+							}
+							doc.fabric_rate = r.message.fabric_rate;
+							doc.price_list_rate = r.message.price_list_rate;
+							doc.applied_rules = r.message.applied_rules || [];
+							dialog.refresh();
+						}
+					}
+				});
+			} else {
+				if (title === 'Check Printing Rate') {
+					doc.base_printing_rate = null;
+					doc.printing_rate = null;
+				} else {
+					doc.base_pretreatment_rate = null;
+					doc.pretreatment_rate = null;
+				}
+				doc.fabric_rate = null;
+				doc.price_list_rate = null;
+				doc.applied_rules = [];
+				dialog.refresh();
+			}
+		}
+
+		const get_fabric_item_name = () => {
+			if (doc.fabric_item) {
+				frappe.db.get_value("Item", doc.fabric_item, "item_name", (r) => {
+					if (r) {
+						dialog.set_value("fabric_item_name", r.item_name);
+					}
+				});
+			} else {
+				dialog.set_value("fabric_item_name", null);
+			}
+		}
+
+		const get_customer_name = () => {
+			if (doc.customer) {
+				frappe.db.get_value("Customer", doc.customer, "customer_name", (r) => {
+					if (r) {
+						dialog.set_value("customer_name", r.customer_name);
+					}
+				});
+			} else {
+				dialog.set_value("customer_name", null);
+			}
+		}
+
+		const dialog = new frappe.ui.Dialog({
+			title: __(title),
+			fields: [
+				{
+					label: __("Fabric Item Code"),
+					fieldname: "fabric_item",
+					fieldtype: "Link",
+					options: "Item",
+					reqd: 1,
+					get_query: () => erpnext.queries.item({
+						textile_item_type: ["in", ["Ready Fabric", "Greige Fabric"]],
+					}),
+					onchange: () => {
+						get_price();
+						get_fabric_item_name();
+					},
+				},
+				{
+					label: __("Fabric Item Name"),
+					fieldname: "fabric_item_name",
+					depends_on: "eval:doc.fabric_item && doc.fabric_item_name != doc.fabric_item",
+					fieldtype: "Data",
+					read_only: 1,
+				},
+				{
+					label: __("Customer"),
+					fieldname: "customer",
+					fieldtype: "Link",
+					options: "Customer",
+					onchange: () => {
+						get_price();
+						get_customer_name();
+					},
+				},
+				{
+					label: __("Customer Name"),
+					fieldname: "customer_name",
+					fieldtype: "Data",
+					depends_on: "eval:doc.customer && doc.customer_name != doc.customer",
+					read_only: 1,
+				},
+				{
+					label: __("Price List"),
+					fieldname: "price_list",
+					fieldtype: "Link",
+					options: "Price List",
+					reqd: 1,
+					get_query: () => {
+						return {
+							filters: {selling: 1}
+						}
+					},
+					onchange: () => {
+						get_price();
+					},
+				},
+				{
+					label: __("Rate"),
+					fieldtype: "Section Break",
+				},
+				{
+					label: __(base_rate_field_label),
+					fieldname: base_rate_field_name,
+					fieldtype: "Currency",
+					read_only: 1,
+				},
+				{
+					label: __(rate_field_label),
+					fieldname: rate_field_name,
+					fieldtype: "Currency",
+					read_only: 1,
+				},
+				{
+					label: __("Fabric Rate"),
+					fieldname: "fabric_rate",
+					fieldtype: "Currency",
+					read_only: 1,
+				},
+				{
+					label: __("Price List Rate"),
+					fieldname: "price_list_rate",
+					fieldtype: "Currency",
+					read_only: 1,
+					bold: 1
+				},
+				{
+					fieldtype: "Section Break",
+				},
+				{
+					label: __("Applied Rules"),
+					fieldname: "applied_rules",
+					fieldtype: "Table",
+					read_only: 1,
+					cannot_add_rows: 1,
+					get_data: () => doc.applied_rules,
+					fields: [
+						{
+							label: __("Rule"),
+							fieldname: "rule",
+							fieldtype: "Link",
+							options: title === "Check Printing Rate" ? "Print Pricing Rule" : "Pretreatment Pricing Rule",
+							read_only: 1,
+							in_list_view: 1
+						},
+						{
+							label: __("Type"),
+							fieldname: "type",
+							fieldtype: "Data",
+							read_only: 1,
+							in_list_view: 1
+						},
+						{
+							label: __("Value"),
+							fieldname: "value",
+							fieldtype: "Float",
+							read_only: 1,
+							in_list_view: 1
+						},
+					]
+				},
+			],
+			doc: doc,
+		});
+		dialog.show();
+	});
+}
