69b6d350a879eb77febfd747025602c36f0fa9b6
Saif Ur Rehman <saif@paralogic.io>
2026-05-13 16:08:48 +0500

    fix(Fabric Packing List): filter fabric items only

--- textile/textile/report/fabric_packing_list/fabric_packing_list.py
+++ textile/textile/report/fabric_packing_list/fabric_packing_list.py
@@ -45,6 +45,7 @@ class FabricPackingList:
 				AND ps.status != 'Unpacked'
 				AND ifnull(psi.source_packing_slip, '') = ''
 				AND psi.qty != 0
+				AND item.textile_item_type IN ('Greige Fabric', 'Ready Fabric', 'Printed Design')
 				{conditions}
 			ORDER BY ps.posting_date, ps.name, psi.idx
 		""".format(conditions=conditions), self.filters, as_dict=1)
